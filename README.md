9c72577c2b3993d3253df8e96f87fa4ad78e59d8
Naman <nnaman33@gmail.com>
2021-09-16 20:34:59 +0530

    This version has multifunctionally and has better efficiency

--- README.md
+++
@@ -1,8 +0,0 @@
-Use this website to modify your text,like capitalize first word, remove punctuation marks, etc.
-This is my first website using Django so the focus is mainly on it and not on front-end.
-This is a fully working website, with front-end and backend.
-Django is used in back-end.
-To run this,
-Goto my-site folder then opwn powershell and run this command
-> python manage.py runserver
-//The website will go live.
---
+++ TEXT_ANALYZER/mysite/views.py
@@ -0,0 +1,58 @@
+# BY NA
+import string
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(req):
+    return render(req,'homepg.html')
+def analyze(req):
+    matter=req.GET.get('text','NAN')
+    orig=matter
+    rem_punc=req.GET.get('rem_punc','OFF')
+    caps=req.GET.get('caps','OFF')
+    delln=req.GET.get('delln','OFF')
+    delsp=req.GET.get('delsp','OFF')
+    ccount=req.GET.get('ccount','OFF')
+    work=''
+    if rem_punc=='on':
+        mat=''
+        for i in matter:
+            if i  not in string.punctuation:
+                mat+=i
+        work+="REMOVED PUNCTUATIONS\n"
+        matter=mat
+    if caps=='on':
+        mat=''
+        mat = ' '.join(ele.capitalize() for ele in matter.replace('\n',' ').split(' '))
+        work+="CAPITALIZED\n"
+        matter=mat
+    if delln=='on':
+        mat=''
+        mat = matter.replace('\n', '').strip()
+        work+="DELETED NEW LINE\n"
+        matter=mat
+    if delsp=='on':
+        mat=''
+        for index,char in enumerate(matter):
+            if not(matter[index]==' ' and matter[index+1]==' '):
+                mat+=char
+        # print(f'{matter}\n {mat}')
+        work+="DELETED EXTRA SPACES\n"
+        matter = mat
+    if ccount=='on':
+        mat=''
+        mat=matter
+        mat += f'\n{len(matter.strip())} AFTER DELETING TERMINATING WHITE SPACES'
+        orig += f'\n{len(orig)} BEFORE DELETING TERMINATING WHITE SPACES'
+        work+="LENGTH OF TEXT\n"
+        matter = mat
+    else:
+        mat=matter
+    dic={
+        'work':work,
+        'old': orig,
+        'new': mat,
+
+    }
+    print(work)
+    return render(req,'work.html',dic)
