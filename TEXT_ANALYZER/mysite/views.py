e69c0e61c47df2199997c5f13d0e1fd60a287a3a
Naman <nnaman33@gmail.com>
2021-11-24 19:52:40 +0530

    Minor Updates : 24/11/21

--- TEXT_ANALYZER/mysite/views.py
+++ TEXT_ANALYZER/mysite/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 def index(req):
     return render(req,'index.html')
 def analyze(req):
-    matter=req.GET.get('text','NAN')
+    matter=req.GET.get('text',None)
     orig=matter
     rem_punc=req.GET.get('rem_punc','OFF')
     caps=req.GET.get('caps','OFF')
@@ -15,43 +15,31 @@ def analyze(req):
     ccount=req.GET.get('ccount','OFF')
     work=''
     if rem_punc=='on':
-        mat=''
-        for i in matter:
-            if i  not in string.punctuation:
-                mat+=i
+        mat=''.join(i for i in matter if i not in string.punctuation)
         work+="REMOVED PUNCTUATIONS\n"
         matter=mat
     if caps=='on':
-        mat=''
         mat = ' '.join(ele.capitalize() for ele in matter.replace('\n',' ').split(' '))
         work+="CAPITALIZED\n"
         matter=mat
     if delln=='on':
-        mat=''
-        mat = matter.replace('\n', '').strip()
+        mat = matter.replace('\n', 'N')
         work+="DELETED NEW LINE\n"
         matter=mat
     if delsp=='on':
-        mat=''
-        for index,char in enumerate(matter):
-            if not(matter[index]==' ' and matter[index+1]==' '):
-                mat+=char
-        # print(f'{matter}\n {mat}')
+        mat=''.join(matter[i] for i in range(len(matter)) if not(matter[i]==' ' and matter[i+1]==' '))
         work+="DELETED EXTRA SPACES\n"
         matter = mat
     if ccount=='on':
-        mat=''
         mat=matter
-        mat += f'\n{len(matter.strip())} AFTER DELETING TERMINATING WHITE SPACES'
-        orig += f'\n{len(orig)} BEFORE DELETING TERMINATING WHITE SPACES'
-        work+="LENGTH OF TEXT\n"
+        mat += f'\nCHAR COUNT: {len(matter)}'
+        orig += f'\nCHAR COUNT: {len(orig)}'
+        work+="CHAR COUNT\n"
         matter = mat
-    else:
-        mat=matter
     dic={
         'work':work,
         'old': orig,
-        'new': mat,
+        'new': matter,
 
     }
     print(work)
